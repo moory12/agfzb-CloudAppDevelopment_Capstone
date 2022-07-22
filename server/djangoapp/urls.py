b90303c43f786aae356594a632dd4b41dc9b163d
Omar Aalem <omaralem82@gmail.com>
2022-07-22 22:03:08 +0300

    lab3 restapis

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     # path for dealer reviews view
 
     # path for add a review view
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -11,6 +11,8 @@ import logging
 import json
 
 # Get an instance of a logger
+from server.djangoapp.restapis import get_dealers_from_cf
+
 logger = logging.getLogger(__name__)
 
 
@@ -79,9 +81,14 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "your-cloud-function-domain/dealerships/dealer-get"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
